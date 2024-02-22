683956b57129b5a1d7936a9b0266ed643dc3e8e5
Asus <jasurbekqochqorov@gmail.com>
2024-02-22 12:25:33 +0500

    homework_16_3

--- lib/Screen/homework_first/CategorieData/categorie_data_screen.dart
+++ lib/Screen/homework_first/CategorieData/categorie_data_screen.dart
@@ -1,61 +1,114 @@
 import 'package:flutter/material.dart';
-import 'package:homework12/data/models/categories/categories.dart';
-import 'package:homework12/data/network_response.dart';
-import 'package:homework12/data/repositories/categories_repository.dart';
+import 'package:homework12/data/models/laptop/laptop-model.dart';
+import 'package:homework12/data/repositories/laptop_repository.dart';
+import 'package:homework12/utils/color/color.dart';
 import 'package:homework12/utils/extension/extension.dart';
+import 'package:homework12/utils/fonts/fonts.dart';
+import '../../../data/network_response.dart';
 
-import '../../../utils/color/color.dart';
-import '../../../utils/fonts/fonts.dart';
-
-
-class CategoriesData extends StatefulWidget {
-  const CategoriesData({super.key, required this.id});
+class CategoriesDataScreen extends StatefulWidget {
+  const CategoriesDataScreen({super.key, required this.id});
   final int id;
-
   @override
-  State<CategoriesData> createState() => _ProductState();
+  State<CategoriesDataScreen> createState() => _LaptopScreenState();
 }
 
-class _ProductState extends State<CategoriesData> {
-  CategoriesRepository categoriesRepository =CategoriesRepository();
+class _LaptopScreenState extends State<CategoriesDataScreen> {
+  final LaptopRepository laptopRepository=LaptopRepository();
+  final List<LaptopModel> products=[];
+
   @override
   Widget build(BuildContext context) {
+    width=MediaQuery.of(context).size.width;
+    height=MediaQuery.of(context).size.height;
     return Scaffold(
       appBar: AppBar(
-        leading:IconButton(onPressed: (){
-          Navigator.pop(context);
-        },icon:const Icon(Icons.arrow_back_outlined,color: AppColors.black,),),
-        title:Text('Product',style: AppTextStyle.interSemiBold.copyWith(
-          color: AppColors.black,fontSize: 24.getW(),
-        ),),
+        backgroundColor: AppColors.white,
+        leading: IconButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          icon: const Icon(
+            Icons.arrow_back_outlined,
+            color: AppColors.black,
+          ),
+        ),
+        title: Text(
+          'Products',
+          style: AppTextStyle.interSemiBold.copyWith(
+            color: AppColors.black,
+            fontSize: 24.getW(),
+          ),
+        ),
         centerTitle: true,
       ),
       body: FutureBuilder(
-        future: categoriesRepository.getCategories(),
+        future: laptopRepository.getLaptops(),
         builder: (
             BuildContext context,
-            AsyncSnapshot<NetworkResponse> snapshot,) {
+            AsyncSnapshot<NetworkResponse> snapshot,
+            ) {
           if (snapshot.hasError) {
             return Center(
               child: Text(snapshot.error.toString()),
             );
           }
           if (snapshot.hasData) {
-            List<CategoriesModel>? categories =
-            (snapshot.data as NetworkResponse).data as List<CategoriesModel>;
-            return Center(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  ClipRRect(
-                      borderRadius: BorderRadius.circular(40.getW()),
-                      child: Image.network(categories[widget.id-1].imageUrl,fit: BoxFit.cover,width: 290.getW(),height:330.getH(),)),
-                  SizedBox(height: 10.getH(),),
-                  Text('Created at:${categories[widget.id-1].createdAt}',style: AppTextStyle.interSemiBold.copyWith(
-                    color: AppColors.black,fontSize: 25.getW(),))
-                ],
-              ),
-            );
+            List<LaptopModel>? laptops =
+            (snapshot.data as NetworkResponse).data
+            as List<LaptopModel>;
+            for(int i=0; i<laptops.length; i++){
+              if(laptops[i].categoryId==widget.id){
+                products.add(laptops[i]);
+              }
+            }
+            return GridView.count(
+                padding: EdgeInsets.symmetric(horizontal: 15.getW(),vertical: 30.getH()),
+                scrollDirection: Axis.vertical,
+                primary: false,
+                crossAxisSpacing: 10.getW(),
+                mainAxisSpacing: 10.getH(),
+                childAspectRatio:0.75,
+                crossAxisCount:2,
+                children:[
+                  ...List.generate(products.length, (index){
+                    // print()
+                    LaptopModel laptopModel=products[index];
+                      return Container(
+                        padding: EdgeInsets.symmetric(horizontal:8.getW(),vertical: 10.getH()),
+                        width:width,
+                        height: height,
+                        decoration: BoxDecoration(
+                            color: AppColors.white,
+                            boxShadow:const[
+                              BoxShadow(
+                                  offset: Offset(0,1),
+                                  spreadRadius: 0,
+                                  blurRadius: 2
+                              )
+                            ],
+                            borderRadius: BorderRadius.circular(20.getW())
+                        ),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text("ID:${laptopModel.id.toString()}",style: AppTextStyle.interSemiBold.copyWith(
+                                color:AppColors.black,fontSize:18.getW()
+                            ),),
+                            Image.network(laptopModel.imageUrl,width: 180.getW(),height:100.getH(),),
+                            const Spacer(),
+                            Text(laptopModel.name,style: AppTextStyle.interSemiBold.copyWith(
+                                color: AppColors.blue,fontSize: 20.getW()
+                            ),),
+                            Text("Price:${laptopModel.price.toString()}\$",style: AppTextStyle.interSemiBold.copyWith(
+                                color:Colors.orange
+                            ),)
+                          ],),
+                      );
+                    }
+                  )
+
+                ]);
           }
           return const Center(
             child: CircularProgressIndicator(),
--- lib/Screen/homework_first/Categories/categories_screen.dart
+++ lib/Screen/homework_first/Categories/categories_screen.dart
@@ -1,13 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:homework12/Screen/Example_second/Product/product.dart';
 import 'package:homework12/Screen/homework_first/CategorieData/categorie_data_screen.dart';
 import 'package:homework12/data/models/categories/categories.dart';
-import 'package:homework12/data/models/laptop/laptop-model.dart';
 import 'package:homework12/data/repositories/categories_repository.dart';
-import 'package:homework12/data/repositories/laptop_repository.dart';
 import 'package:homework12/utils/color/color.dart';
 import 'package:homework12/utils/extension/extension.dart';
 import 'package:homework12/utils/fonts/fonts.dart';
+import 'package:zoom_tap_animation/zoom_tap_animation.dart';
 
 import '../../../data/network_response.dart';
 
@@ -19,26 +17,38 @@ class CategoriesScreen extends StatefulWidget {
 }
 
 class _LaptopScreenState extends State<CategoriesScreen> {
-  final CategoriesRepository categoriesRepository =CategoriesRepository();
+  final CategoriesRepository categoriesRepository = CategoriesRepository();
 
   @override
   Widget build(BuildContext context) {
+    width=MediaQuery.of(context).size.width;
+    height=MediaQuery.of(context).size.height;
     return Scaffold(
       appBar: AppBar(
-        leading:IconButton(onPressed: (){
-          Navigator.pop(context);
-        },icon:const Icon(Icons.arrow_back_outlined,color: AppColors.black,),),
-        title:Text('Categories',style: AppTextStyle.interSemiBold.copyWith(
-          color: AppColors.black,fontSize: 24.getW(),
-        ),),
+        leading: IconButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          icon: const Icon(
+            Icons.arrow_back_outlined,
+            color: AppColors.black,
+          ),
+        ),
+        title: Text(
+          'Categories',
+          style: AppTextStyle.interSemiBold.copyWith(
+            color: AppColors.black,
+            fontSize: 24.getW(),
+          ),
+        ),
         centerTitle: true,
       ),
-      body:FutureBuilder(
+      body: FutureBuilder(
         future: categoriesRepository.getCategories(),
         builder: (
-            BuildContext context,
-            AsyncSnapshot<NetworkResponse> snapshot,
-            ) {
+          BuildContext context,
+          AsyncSnapshot<NetworkResponse> snapshot,
+        ) {
           if (snapshot.hasError) {
             return Center(
               child: Text(snapshot.error.toString()),
@@ -46,40 +56,60 @@ class _LaptopScreenState extends State<CategoriesScreen> {
           }
           if (snapshot.hasData) {
             List<CategoriesModel>? categories =
-            (snapshot.data as NetworkResponse).data as List<CategoriesModel>;
-            return ListView(
-              children: List.generate(
-                categories.length,
-                    (index) {
-                  CategoriesModel categoriesModel = categories[index];
-                  return Padding(
-                    padding:EdgeInsets.symmetric(horizontal:10.getW(),vertical:5.getH()),
-                    child: ListTile(
-                        tileColor: AppColors.white,
-                        splashColor: AppColors.blue,
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(16.getW()),
-                        ),
+                (snapshot.data as NetworkResponse).data
+                    as List<CategoriesModel>;
+            return GridView.count(
+              padding: EdgeInsets.symmetric(horizontal: 15.getW(),vertical: 30.getH()),
+                scrollDirection: Axis.vertical,
+                primary: false,
+                crossAxisSpacing: 10.getW(),
+                mainAxisSpacing: 10.getH(),
+                childAspectRatio:0.7,
+                crossAxisCount:2,
+                children:[
+                  ...List.generate(categories.length, (index){
+                    CategoriesModel categoriesModel=categories[index];
+                    return ZoomTapAnimation(
                         onTap: (){
-                          Navigator.push(context,MaterialPageRoute(builder: (context){
-                            return CategoriesData(id:categoriesModel.id,);
+                          Navigator.push(context, MaterialPageRoute(builder: (context){
+                            return CategoriesDataScreen(id:categoriesModel.id);
                           }));
                         },
-                        title:Row(children: [
-                          Text('ID:'+categoriesModel.id.toString(),style: AppTextStyle.interSemiBold.copyWith(
-                            color: AppColors.black,fontSize:18.getW(),
+                      child: Container(
+                        padding: EdgeInsets.symmetric(horizontal:8.getW(),vertical: 10.getH()),
+                        width:width,
+                        height: height,
+                        decoration: BoxDecoration(
+                          color: AppColors.white,
+                          boxShadow:const[
+                            BoxShadow(
+                              offset: Offset(0,1),
+                              spreadRadius: 0,
+                              blurRadius: 2
+                            )
+                          ],
+                          borderRadius: BorderRadius.circular(20.getW())
+                        ),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                          Text("ID:${categoriesModel.id.toString()}",style: AppTextStyle.interSemiBold.copyWith(
+                            color:AppColors.black,fontSize:18.getW()
                           ),),
-                          SizedBox(width: 10.getW(),),
-                          Text(categoriesModel.name,
-                            style: AppTextStyle.interSemiBold.copyWith(
-                              color: AppColors.black,fontSize: 18.getW(),),
-                          ),
-                        ],)
-                    ),
-                  );
-                },
-              ),
-            );
+                          Image.network(categoriesModel.imageUrl,width: 180.getW(),height:100.getH(),),
+                          const Spacer(),
+                            Text(categoriesModel.name,style: AppTextStyle.interSemiBold.copyWith(
+                            color: AppColors.blue,fontSize: 20.getW()
+                          ),),
+                          Text("created at: ${categoriesModel.createdAt}",style: AppTextStyle.interSemiBold.copyWith(
+                            color:Colors.orange
+                          ),)
+                        ],),
+                      ),
+                    );
+                  })
+
+                ]);
           }
           return const Center(
             child: CircularProgressIndicator(),
