32b19750eeff4e146199b311d279022dad0b0c2d
Asus <jasurbekqochqorov@gmail.com>
2024-03-29 19:59:07 +0500

    homework_16_3

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,20 @@
 import 'package:flutter/material.dart';
-import 'package:homework12/Screen/main_screen.dart';
+import 'package:homework12/screens/splash_screen.dart';
+import 'package:homework12/view_models/location_view_model.dart';
+import 'package:homework12/view_models/maps_view_model.dart';
+import 'package:provider/provider.dart';
 
-void main(){
-  runApp(const MyApp());
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
 
+  runApp(MultiProvider(
+    providers: [
+      ChangeNotifierProvider(create: (_) => LocationViewModel()),
+      ChangeNotifierProvider(create: (_) => MapsViewModel()),
+    ],
+    child: MyApp(),
+  ));
 }
-
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -13,8 +22,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: MainScreen(),
       debugShowCheckedModeBanner: false,
+      theme: ThemeData(useMaterial3: false),
+      home: const SplashScreen(),
     );
   }
 }
