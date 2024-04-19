45efc98aeea65a7ed0e58c23dcced123a3ff3aa1
Asus <jasurbekqochqorov@gmail.com>
2024-04-19 10:39:13 +0500

    homework_16_3

--- lib/Screen/main_screen.dart
+++ lib/Screen/main_screen.dart
@@ -1,13 +1,16 @@
 import 'package:flutter/material.dart';
 
-
 class MainScreen extends StatelessWidget {
   const MainScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        title: Text("Game"),
+        centerTitle: true,
+      ),
+      body:Center(child: Text(""),)
     );
   }
 }
